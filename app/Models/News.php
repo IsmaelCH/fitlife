e9f19683be352cd6394e374825a0a17242f22a4a
IsmaelCH <ismael.charia.jimenez@student.ehb.be>
2025-12-03 16:14:31 +0100

    Add models and relationships
    Add models and relationships

---
+++ app/Models/News.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'image_path',
+        'content',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'news_tag');
+    }
+}
